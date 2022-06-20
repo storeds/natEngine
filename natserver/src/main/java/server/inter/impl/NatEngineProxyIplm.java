52a2215e7aead593942e3514e1c13280806733ba
cx <1141413372@qq.com>
2022-06-20 21:42:14 +0800

    服务端提供外部接口

---
+++ natserver/src/main/java/server/inter/impl/NatEngineProxyIplm.java
@@ -0,0 +1,16 @@
+package server.inter.impl;
+
+import server.inter.NatEngineProxy;
+
+/**
+ * @program: NatEngine
+ * @author: cx
+ * @create: 2022-06-20 21:40
+ * @description:  外部代理类
+ **/
+public class NatEngineProxyIplm  implements NatEngineProxy {
+    @Override
+    public void start() {
+
+    }
+}

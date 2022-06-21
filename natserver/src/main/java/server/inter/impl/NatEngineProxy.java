697d7da2d93774083ef1cfc1b8278be4224cbd6e
cx <1141413372@qq.com>
2022-06-21 22:05:19 +0800

    添加启动代理

---
+++ natserver/src/main/java/server/inter/impl/NatEngineProxy.java
@@ -0,0 +1,23 @@
+package server.inter.impl;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * @program: NatEngine
+ * @author: cx
+ * @create: 2022-06-21 21:23
+ * @description:  创建代理server
+ **/
+@Slf4j
+public class NatEngineProxy  implements InvocationHandler {
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+        log.info("启动后端服务");
+
+        return null;
+    }
+}
